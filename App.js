c97117afdaf852fd073ddae0865b741e806c893d
regevbs <regevbensi@gmail.com>
2018-11-27 22:30:51 +0200

    Added some react native elements

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Card, ListItem, Button,ButtonGroup } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,16 +11,46 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    index: 0
+  }
+
+  updateIndex = (index) => {
+    this.setState({index})
+  }
+
   render() {
+    let pic = {
+      uri: 'https://en.wikipedia.org/wiki/Main_Page#/media/File:TSM350_2015_-_Joey_Logano_1_-_Stierch.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native waddup alex!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+      <Icon.Button name="facebook" backgroundColor="#3b5998">
+        <Text style={{fontFamily: 'Arial', fontSize: 18, backgroundColor:"#3b5998"}}>Login with Facebook</Text>
+      </Icon.Button>
+      <Icon.Button name="google" backgroundColor="#f18973">
+        <Text style={{fontFamily: 'Arial', fontSize: 18, backgroundColor:"#f18973"}}>Login with Google</Text>
+      </Icon.Button>
+      <ButtonGroup
+        selectedBackgroundColor="blue"
+        underlayColor = "blue"
+        onPress={this.updateIndex}
+        selectedIndex={this.state.index}
+        buttons={['על הסטודיו','העמוד שלי','הרשמה לשיעורים','הודעות']}
+        containerStyle={{height: 50}}
+        textStyle = {styles.welcome}
+
+         />
+
+
       </View>
     );
+
+
   }
 }
 
@@ -37,7 +62,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 18,
     textAlign: 'center',
     margin: 10,
   },
